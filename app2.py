30809f3de8e8ec6e6edea01fb1af3d25e21526c4
KamelABA <123214040+KamelABA@users.noreply.github.com>
2024-09-11 17:58:09 +0100

    http://128.214.253.82/

--- app2.py
+++ app2.py
@@ -588,4 +588,4 @@ def get_apple_disease(prediction):
             *get_Supplement_info("Apple : Healthy")
         ]
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='128.214.253.82', port=5000)
